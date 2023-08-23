a0757043a4240cbd9c961f290954e6b9088d7c79
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2023-08-23 13:39:39 -0700

    update pre-commit config (#7)
    Update pre-commit configuration to use ruff as the default linter. Increased line width of ruff to 120. Kind of works for most scenarios.
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/test_dense.py
+++ tests/test_dense.py
@@ -5,11 +5,13 @@ __author__ = "ltla, jkanche"
 __copyright__ = "ltla, jkanche"
 __license__ = "MIT"
 
+
 def test_dense():
     y = np.random.rand(1000, 100)
     ptr = tatamize(y)
     assert all(ptr.row(0) == y[0, :])
     assert all(ptr.column(1) == y[:, 1])
+
 
 def test_numpy_with_dtype():
     y = (np.random.rand(50, 12) * 100).astype("i8")
@@ -17,9 +19,10 @@ def test_numpy_with_dtype():
     assert all(ptr.row(0) == y[0, :])
     assert all(ptr.column(1) == y[:, 1])
 
+
 def test_dense_column_major():
-    y = np.ndarray((1000, 100), order = "F")
-    y[:,:] = np.random.rand(1000, 100)
+    y = np.ndarray((1000, 100), order="F")
+    y[:, :] = np.random.rand(1000, 100)
     assert y.flags["F_CONTIGUOUS"]
     ptr = tatamize(y)
     assert all(ptr.row(0) == y[0, :])
